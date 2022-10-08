f0cd1dab2add2c7e14580e85bbbbb3ee10292eae
Felipe Silva <felipemosilvaa@gmail.com>
2022-10-08 02:27:24 -0300

    feature: config routes; config conection mongo atlas and add controller functions

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,35 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import User from "../database/schemas/User";
 class UserController {
 
+  async find(request: Request, response: Response) {
+    try {
+      const users = await User.find();
+      return response.json(users);
+    } catch (error) {
+      return response.status(500).send({
+        error: "Falha ao buscar",
+        message: error,
+      });
+    }
+  }
+
+  async create(request: Request, response: Response) {
+    const { name, lastName, email } = request.body;
+    try {
+      const user = await User.create({
+        name,
+        lastName,
+        email,
+      });
+      return response.json(user);
+    } catch (error) {
+      return response.status(500).send({
+        error: "Falha ao registrar",
+        message: error,
+      });
+    }
+  }
 }
+
+export default new UserController();
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import routes from "./routes";
 
 const app = express();
-mongoose.connect('mongodb://')
+mongoose.connect('mongodb+srv://fmsilva:24636112@cluster0.csvyt3s.mongodb.net/?retryWrites=true&w=majority')
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('rodou')
 })
-app.use(express.json());
