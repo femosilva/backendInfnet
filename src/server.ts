878de201da6d7b192b0c7620c887b5a0e406b3e1
Felipe Silva <felipemosilvaa@gmail.com>
2022-11-04 14:21:39 -0300

    Conclusão do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./routes";
 
 const app = express();
-mongoose.connect('mongodb+srv://fmsilva:24636112@cluster0.csvyt3s.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(
+  "mongodb+srv://fmsilva:24636112@cluster0.csvyt3s.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200
+  })
+);
 app.use(routes);
-
-app.listen(3000, () => {
-    console.log('rodou')
-})
+app.listen(3000);
